d27c107a815706220ec69fb22a92c9f686fa8a75
meghna-dm <meghna.dm@ikea.com>
2019-07-01 10:52:55 +0200

    API path changed

--- src/main/java/com/ikea/ipay/paymentTransaction/restapi/PaymentsTransactionRESTAPI.java
+++ src/main/java/com/ikea/ipay/paymentTransaction/restapi/PaymentsTransactionRESTAPI.java
@@ -5,12 +5,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ikea.ipay.paymentTransaction.entity.Card;
 import com.ikea.ipay.paymentTransaction.service.PaymentTransactionService;
 
 @RestController
+@RequestMapping("PaymentTransaction")
 public class PaymentsTransactionRESTAPI {
 	
 	@Autowired PaymentTransactionService paymentTransactionservice;
